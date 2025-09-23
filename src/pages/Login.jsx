78bb159824b4018c2fc2dbc222b1c1dc7dd701f2
Jhay-R Z. Cervantes <jhayrcervantes0981@gmail.com>
2025-09-23 10:12:18 +0800

    initial commit

---
+++ src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import '../assets/styles/Login&Register.css';
+
+function Login() {
+    return (
+        <>
+            <h1>Hello World</h1>
+            <p>dadawdawdwa</p>
+        </>
+    );
+}
+
+export default Login;
